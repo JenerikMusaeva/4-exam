c0211c9e08ce1b8bc65c2207b110022991308c2a
Jenerik <proximity2006@gmail.com>
2021-02-10 22:33:45 +0600

    add style for responsive

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
 const taskForm = document.getElementById('task-form');
 const btnAddTask = document.getElementById('saveTask');
 const tasksDiv = document.getElementById('tasks');
+// const doneTask = document.getElementsById('task-done')
+// const deleteTask = document.getElementById('delete-task');
 
-// let addItem = (listElement) => {
-//   return function (title, description) {
-//     let $task = document.createElement('div');
+//создание сообщения ошибки
+const addError = (errorMessage) => {
+  let msg = document.createElement('div');
+  msg.className = 'alert alert-danger';
+  msg.innerHTML = errorMessage;
 
-//     $task.classList.add('task-item');
-
-//     $task.innerHTML = `
-//     <div class='task-content'>
-//     <h3>${title}</h3>
-//     <p>${description}</p>
-//     </div>
-//     <div class='task-icon'>
-//     <i class='icon-check'></i>
-//     <i class='delete-task' id='delete-task'></i>
-//     </div>
-//     `
+  document.getElementById('errors').append(msg);
+};
 
 
-//     listElement.prepend($task);
-//   }
-// }
-
-// let addTask = addItem(tasksDiv); 
-
+//создание записи
 btnAddTask.addEventListener('click', e => {
   e.preventDefault();
 
   const _taskTitle = taskForm.querySelector('[name=task-title]').value
   const _taskDescription = taskForm.querySelector('[name=task-description]').value
+
+  document.getElementById('errors').innerHTML = '';
+
+  //валидация заголовка
+  if (_taskTitle.length === '') {
+    addError('Укажите заголовок')
+  } 
 
   let $task = document.createElement('div');
 
@@ -42,7 +38,7 @@ btnAddTask.addEventListener('click', e => {
     <p>${_taskDescription}</p>
     </div>
     <div class='task-icon'>
-    <i class='icon-check'></i>
+    <i class='icon-check' id='task-done'></i>
     <i class='delete-task' id='delete-task'></i>
     </div>
     `
@@ -50,6 +46,23 @@ btnAddTask.addEventListener('click', e => {
     tasksDiv.prepend($task);
 
   // taskForm.reset();
+
   
+ 
 });
+
+// перечеркивание записи
+// doneTask.addEventListener('click', e => {
+//  e.target.closest('item-task).remove()
+
+
+
+// удаление записи
+// deleteTask.addEventListener('click', e => {
+//   e.target.closest('item-task).remove()
+
+
+//   console.log(deleteTask)
+
+// });
 
