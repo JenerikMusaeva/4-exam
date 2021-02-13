55a9981db46f451452d0339c333d00a810a5ea00
Jenerik <proximity2006@gmail.com>
2021-02-13 19:22:57 +0600

    add some style for task list

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
-const taskForm = document.getElementById('task-form');
-const btnAddTask = document.getElementById('saveTask');
-const tasksDiv = document.getElementById('tasks');
+const taskForm = document.getElementById('task-form')
+const btnAddTask = document.getElementById('saveTask')
+const tasksDiv = document.getElementById('tasks')
 // const doneTask = document.getElementsById('task-done')
-const modalDeleteTask = document.getElementById('modalDeleteTask');
-
+const modalDeleteTask = document.getElementById('modalDeleteTask')
 
 //создание сообщения ошибки
-const addError = (errorMessage) => {
-
-  let msg = document.createElement('div');
-  msg.className = 'alert alert-danger';
-  msg.innerHTML = errorMessage;
-
-  document.getElementById('errors').append(msg);
-};
+const addError = errorMessage => {
+  let msg = document.createElement('div')
+  msg.className = 'alert alert-danger'
+  msg.innerHTML = errorMessage
+  document.getElementById('errors').append(msg)
+}
 
 btnAddTask.addEventListener('click', e => {
-  e.preventDefault();
+  e.preventDefault()
 
   const _taskTitle = taskForm.querySelector('[name=task-title]')
   const _taskDescription = taskForm.querySelector('[name=task-description]')
 
-  document.getElementById('errors').innerHTML = '';
+  document.getElementById('errors').innerHTML = ''
 
   //валидация заголовка
   if (_taskTitle.value === '') {
     addError('Укажите заголовок')
-    return 
-  } 
+    return
+  }
 
   // создание записи
-  let $task = document.createElement('div');
+  let $task = document.createElement('div')
 
-    $task.classList.add('task-item');
+  $task.classList.add('task-item')
 
-    $task.innerHTML = `
+  $task.innerHTML = `
     <div class="task-leftside">
     <div class='task-content'>
     <h3>${_taskTitle.value}</h3>
@@ -43,67 +40,52 @@ btnAddTask.addEventListener('click', e => {
     <span class="task-edit" id='task-edit'><i class='icon-edit' ></i> изменить</span>
     </div>
     <div class='task-icon'>
-    <i class='icon-check' id='task-done'></i>
+    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
     <i class='delete-task' id='delete-task'></i>
     </div>
-    `;
+    `
 
-    tasksDiv.prepend($task);
-    
+  tasksDiv.prepend($task)
 
-    $('#createTaskModal').modal('hide');
+  $('#createTaskModal').modal('hide')
 
-    //  очищение полей модалки создания записи
-    _taskTitle.value = '';
-    _taskDescription.value = '';
+  //  очищение полей модалки создания записи
+  _taskTitle.value = ''
+  _taskDescription.value = ''
 
-    // скрипт для удаления записи
-    const deleteTask = $task.querySelector('#delete-task')
-    deleteTask.addEventListener('click', e =>{
-      $('#deleteTaskModal').modal('show');
-      modalDeleteTask.addEventListener('click', e =>{
-        $task.remove();
-        $('#deleteTaskModal').modal('hide');
-      })
-      
+  // скрипт для удаления записи
+  const deleteTask = $task.querySelector('#delete-task')
+  deleteTask.addEventListener('click', e => {
+    $('#deleteTaskModal').modal('show')
+    modalDeleteTask.addEventListener('click', e => {
+      $task.remove()
+      $('#deleteTaskModal').modal('hide')
     })
+  })
 
-    //скрипт для изменение записи 
-    const editTask = $task.querySelector('#task-edit');
-    editTask.addEventListener('click', e => {
-      
-      $('#editTaskModal').modal('show');
+  //скрипт для изменение записи
+  const editTask = $task.querySelector('#task-edit')
+  editTask.addEventListener('click', e => {
+    $('#editTaskModal').modal('show')
 
-      const $modalEdit = document.querySelector('#editTaskModal');
+    const $modalEdit = document.querySelector('#editTaskModal')
 
-      const $innerTitle = $modalEdit.querySelector('#edit-task-title');
-      const $innerDescription = $modalEdit.querySelector('#task-description');
+    const $innerTitle = $modalEdit.querySelector('#edit-task-title')
+    const $innerDescription = $modalEdit.querySelector('#task-description')
 
-      const _editTitle = $task.querySelector('h3');
-      const _editDescription = $task.querySelector('p');
-      
-      $innerTitle.value = _editTitle.innerText;
-      $innerDescription.value = _editDescription.innerText;
+    const _editTitle = $task.querySelector('h3')
+    const _editDescription = $task.querySelector('p')
 
-      const btnSaveEditTask = $modalEdit.querySelector('#btnEditTask');
+    $innerTitle.value = _editTitle.innerText
+    $innerDescription.value = _editDescription.innerText
 
-      btnSaveEditTask.addEventListener('click', e => {
+    const btnSaveEditTask = $modalEdit.querySelector('#btnEditTask')
 
-      _editTitle.innerText = $innerTitle.value;
-      _editDescription.innerText = $innerDescription.value;
+    btnSaveEditTask.addEventListener('click', e => {
+      _editTitle.innerText = $innerTitle.value
+      _editDescription.innerText = $innerDescription.value
 
-      $('#editTaskModal').modal('hide');
-        
-      })
-
+      $('#editTaskModal').modal('hide')
     })
-
-    
-
-
-
-
- 
-});
-
-
+  })
+})
